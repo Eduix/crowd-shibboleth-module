72d415f49dc6ef486fff3ff3c1eff02f5be04e65
Juha Leppälä <juha@eduix.fi>
2018-09-24 13:42:40 +0300

    Fix XSS vulnerability in the 'not permitted' error page

--- nordunet-sso/src/main/java/net/nordu/crowd/sso/NORDUnetHtmlServlet.java
+++ nordunet-sso/src/main/java/net/nordu/crowd/sso/NORDUnetHtmlServlet.java
@@ -23,18 +23,21 @@ import java.util.List;
 import javax.servlet.http.HttpServlet;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
+import org.owasp.html.HtmlPolicyBuilder;
+import org.owasp.html.PolicyFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * <p>Base for NORDUnet HTML producing servlets. Has a default skin and template parts
- * that can be overridden.</p>
- * <p>Override files:
+ * <p>
+ * Base for NORDUnet HTML producing servlets. Has a default skin and template parts that can be overridden.</p>
+ * <p>
+ * Override files:
  * <ul>
  * <li> Html fragment for the header is read from %crowd-home%/ssoservlet/header.html</li>
  * <li> Html fragment for the footer is read from %crowd-home%/ssoservlet/footer.html</li>
- * <li> Plugin module keys for theme web resources are read from %crowd-home%/ssoservlet/themeResources.txt - 
- * to include multiple web resources out them on separate lines. You need to install plugins that provide the plugin modules
+ * <li> Plugin module keys for theme web resources are read from %crowd-home%/ssoservlet/themeResources.txt - to include
+ * multiple web resources out them on separate lines. You need to install plugins that provide the plugin modules
  * <li> If these files are missing or can't be read then default NORDUnet values are used
  * </p>
  *
@@ -43,111 +46,112 @@ import org.slf4j.LoggerFactory;
  */
 public abstract class NORDUnetHtmlServlet extends HttpServlet {
 
-    private static final Logger log = LoggerFactory.getLogger(NORDUnetHtmlServlet.class);
-    private static final String BODY_START_HTML_FRAGMENT = "header.html";
-    private static final String BODY_END_HTML_FRAGMENT = "footer.html";
-    private static final String THEME_RESOURCES = "themeResources.txt";
-    private WebResourceManager webResourceManager;
-    private HomeLocator homeLocator;
+   private static final Logger log = LoggerFactory.getLogger(NORDUnetHtmlServlet.class);
+   private static final String BODY_START_HTML_FRAGMENT = "header.html";
+   private static final String BODY_END_HTML_FRAGMENT = "footer.html";
+   private static final String THEME_RESOURCES = "themeResources.txt";
+   private final WebResourceManager webResourceManager;
+   private final HomeLocator homeLocator;
+   private static final PolicyFactory POLICY_FACTORY = new HtmlPolicyBuilder().toFactory();
 
-    public NORDUnetHtmlServlet(WebResourceManager webResourceManager, HomeLocator homeLocator) {
-        this.webResourceManager = webResourceManager;
-        this.homeLocator = homeLocator;
-    }
+   public NORDUnetHtmlServlet(WebResourceManager webResourceManager, HomeLocator homeLocator) {
+      this.webResourceManager = webResourceManager;
+      this.homeLocator = homeLocator;
+   }
 
-    private String getHtmlFragment(String fragmentName) {
-        File fragmentFile = new File(homeLocator.getHomePath() + File.separatorChar + "ssoservlet" + File.separatorChar + fragmentName);
-        if (fragmentFile.exists() && fragmentFile.canRead()) {
-            try {
-                return FileUtils.readFileToString(fragmentFile);
-            } catch (IOException e) {
-                log.warn("Could not read html fragment {}: {}", fragmentFile.getAbsolutePath(), e.getMessage());
-            }
-        }
-        return null;
-    }
+   private String getHtmlFragment(String fragmentName) {
+      File fragmentFile = new File(homeLocator.getHomePath() + File.separatorChar + "ssoservlet" + File.separatorChar + fragmentName);
+      if (fragmentFile.exists() && fragmentFile.canRead()) {
+         try {
+            return FileUtils.readFileToString(fragmentFile);
+         } catch (IOException e) {
+            log.warn("Could not read html fragment {}: {}", fragmentFile.getAbsolutePath(), e.getMessage());
+         }
+      }
+      return null;
+   }
 
-    private List<String> getThemeResources() {
-        File themeResourcesFile = new File(homeLocator.getHomePath() + File.separatorChar + "ssoservlet" + File.separatorChar + THEME_RESOURCES);
-        if (themeResourcesFile.exists() && themeResourcesFile.canRead()) {
-            try {
-                return FileUtils.readLines(themeResourcesFile);
-            } catch (IOException e) {
-                log.warn("Could not read theme resources file {}: {}", themeResourcesFile.getAbsolutePath(), e.getMessage());
-            }
-        }
-        // If we could not find customised theme resources return NORDUnet servlet skin
-        return Collections.singletonList("net.nordu.crowd.nordunet-sso:servletSkin");
-    }
+   private List<String> getThemeResources() {
+      File themeResourcesFile = new File(homeLocator.getHomePath() + File.separatorChar + "ssoservlet" + File.separatorChar + THEME_RESOURCES);
+      if (themeResourcesFile.exists() && themeResourcesFile.canRead()) {
+         try {
+            return FileUtils.readLines(themeResourcesFile);
+         } catch (IOException e) {
+            log.warn("Could not read theme resources file {}: {}", themeResourcesFile.getAbsolutePath(), e.getMessage());
+         }
+      }
+      // If we could not find customised theme resources return NORDUnet servlet skin
+      return Collections.singletonList("net.nordu.crowd.nordunet-sso:servletSkin");
+   }
 
-    protected void writeHtml(PrintWriter writer, String title, String content) {
-        writeHtmlStart(writer, title, null);
-        writer.write(content);
-        writeHtmlEnd(writer);
-    }
+   protected void writeHtml(PrintWriter writer, String title, String content) {
+      writeHtmlStart(writer, title, null);
+      writer.write(content);
+      writeHtmlEnd(writer);
+   }
 
-    protected void writeHtmlStart(PrintWriter writer, String title, List<String> errors) {
-        writeHtmlStart(writer, title, errors, null);
-    }
-    
-    protected void writeCustomError(PrintWriter writer, String title, ServletError error, String errorText) {
-       writeHtmlStart(writer, title, Collections.singletonList(errorText));
-       String errorFragment = getHtmlFragment(error.getFragment());
-       if(errorFragment != null) {
-          writer.write(errorFragment);
-       } else {
-          writer.write(error.getDefaultContent());
-       }
-       writeHtmlEnd(writer);
-    }
+   protected void writeHtmlStart(PrintWriter writer, String title, List<String> errors) {
+      writeHtmlStart(writer, title, errors, null);
+   }
 
-    protected void writeHtmlStart(PrintWriter writer, String title, List<String> errors, String headInclude) {
-        writer.write("<html><head><title>" + title + "</title>");
-        webResourceManager.requireResource("com.atlassian.auiplugin:ajs");
-        for (String themeResource : getThemeResources()) {
-            if (!StringUtils.isBlank(themeResource)) {
-                webResourceManager.requireResource(themeResource);
-            }
-        }
-        if (requiresResources()) {
-            for (String resource : getRequiredResources()) {
-                webResourceManager.requireResource(resource);
-            }
-        }
-        webResourceManager.includeResources(writer, UrlMode.AUTO);
-        if (headInclude != null) {
-            writer.write(headInclude);
-        }
-        writer.write("</head><body>");
-        String htmlFragment = getHtmlFragment(BODY_START_HTML_FRAGMENT);
-        if (htmlFragment != null) {
-            writer.write(htmlFragment);
-        } else {
-            writer.write("<div id='container'><div id='top'><h1 title='NORDUnet'>NORDUnet</h1></div><div id='content'>");
-        }
-        if (errors != null && !errors.isEmpty()) {
-            writer.write("<div style='padding: 2px; background: #fcc; border:5px solid #f00; font-weight:bold;'>");
-            writer.write("<p>Errors:</p>");
-            writer.write("<ul>");
-            for (String error : errors) {
-                writer.append("<li>").append(error).append("</li>");
-            }
-            writer.write("</ul></div>");
-        }
-    }
+   protected void writeCustomError(PrintWriter writer, String title, ServletError error, String errorText) {
+      writeHtmlStart(writer, title, Collections.singletonList(errorText));
+      String errorFragment = getHtmlFragment(error.getFragment());
+      if (errorFragment != null) {
+         writer.write(errorFragment);
+      } else {
+         writer.write(error.getDefaultContent());
+      }
+      writeHtmlEnd(writer);
+   }
 
-    public abstract String[] getRequiredResources();
+   protected void writeHtmlStart(PrintWriter writer, String title, List<String> errors, String headInclude) {      
+      writer.write("<html><head><title>" + POLICY_FACTORY.sanitize(title) + "</title>");
+      webResourceManager.requireResource("com.atlassian.auiplugin:ajs");
+      for (String themeResource : getThemeResources()) {
+         if (!StringUtils.isBlank(themeResource)) {
+            webResourceManager.requireResource(themeResource);
+         }
+      }
+      if (requiresResources()) {
+         for (String resource : getRequiredResources()) {
+            webResourceManager.requireResource(resource);
+         }
+      }
+      webResourceManager.includeResources(writer, UrlMode.AUTO);
+      if (headInclude != null) {
+         writer.write(headInclude);
+      }
+      writer.write("</head><body>");
+      String htmlFragment = getHtmlFragment(BODY_START_HTML_FRAGMENT);
+      if (htmlFragment != null) {
+         writer.write(htmlFragment);
+      } else {
+         writer.write("<div id='container'><div id='top'><h1 title='NORDUnet'>NORDUnet</h1></div><div id='content'>");
+      }
+      if (errors != null && !errors.isEmpty()) {
+         writer.write("<div style='padding: 2px; background: #fcc; border:5px solid #f00; font-weight:bold;'>");
+         writer.write("<p>Errors:</p>");
+         writer.write("<ul>");
+         for (String error : errors) {
+            writer.append("<li>").append(POLICY_FACTORY.sanitize(error)).append("</li>");
+         }
+         writer.write("</ul></div>");
+      }
+   }
 
-    public abstract boolean requiresResources();
+   public abstract String[] getRequiredResources();
 
-    protected void writeHtmlEnd(PrintWriter writer) {
-        String htmlFragment = getHtmlFragment(BODY_END_HTML_FRAGMENT);
-        if (htmlFragment != null) {
-            writer.write(htmlFragment);
-        } else {
-            writer.write("</div><div id='footer'>");
-            writer.write("<p>NORDUnet A/S | Kastruplundgade 22 | DK-2770 Kastrup | DENMARK | Phone +45 32 46 25 00 | Fax +45 45 76 23 66 | info@nordu.net</p></div></div>");
-        }
-        writer.write("</body></html>");
-    }
+   public abstract boolean requiresResources();
+
+   protected void writeHtmlEnd(PrintWriter writer) {
+      String htmlFragment = getHtmlFragment(BODY_END_HTML_FRAGMENT);
+      if (htmlFragment != null) {
+         writer.write(htmlFragment);
+      } else {
+         writer.write("</div><div id='footer'>");
+         writer.write("<p>NORDUnet A/S | Kastruplundgade 22 | DK-2770 Kastrup | DENMARK | Phone +45 32 46 25 00 | Fax +45 45 76 23 66 | info@nordu.net</p></div></div>");
+      }
+      writer.write("</body></html>");
+   }
 }
